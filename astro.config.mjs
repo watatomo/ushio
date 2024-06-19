58e9d87b47f5d1efe1a37bc29141b9b800efa307
Ren <hodorugi@gmail.com>
2024-06-18 18:41:43 -0700

    Start working on nav component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), icon()],
-  trailingSlash: "never"
+    site: "https://example.com",
+    integrations: [mdx(), sitemap(), icon()],
 });
