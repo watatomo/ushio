5e1d43310787a66d009ad9daafcdb83fb75dd82f
yuu <58155530+kagiura@users.noreply.github.com>
2024-06-21 00:03:13 +0700

    [build] added react, implemented basic state controller & types
    Co-Authored-By: Ren <23179278+watatomo@users.noreply.github.com>

---
+++ src/components/tl/StoryController.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+function StoryController({ series, children }: { children: ReactNode }) {
+    
+    const 
+    return <div>{children}</div>;
+}
+
+export default StoryController;
---
+++ src/types/stories.ts
@@ -0,0 +1,18 @@
+type Series = "18TRIP" | "enstars" | "prsk";
+
+interface StoryOptions18TRIP {
+    firstName: string;
+    lastName: string;
+    gender: "male" | "female";
+}
+
+type StoryOptions =
+    | { series: "18TRIP"; options: StoryOptions18TRIP }
+    | {
+          series: "enstars";
+          options: never;
+      }
+    | {
+          series: "prsk";
+          options: never;
+      };
