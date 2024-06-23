77afda4073f853535f296a49efccdcf338829b60
Ren <hodorugi@gmail.com>
2024-06-23 00:44:41 -0700

    Styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), icon(), react()]
+    site: "https://yukikaze.love",
+    integrations: [mdx(), sitemap(), icon(), react()],
 });
